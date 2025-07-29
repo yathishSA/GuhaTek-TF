bd8acf8babdc18e593db37b7122308c0e1efdc4b
yathish <yathish1047@gmail.com>
2025-07-29 15:26:42 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,183 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "Guhatek_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Guhatek-vpc"
+  }
+}
+
+resource "aws_subnet" "Guhatek_subnet" {
+  count = 2
+  vpc_id                  = aws_vpc.Guhatek_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.Guhatek_vpc.cidr_block, 8, count.index)
+  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Guhatek-subnet-${count.index}"
+  }
+}
+
+resource "aws_internet_gateway" "Guhatek_igw" {
+  vpc_id = aws_vpc.Guhatek_vpc.id
+
+  tags = {
+    Name = "Guhatek-igw"
+  }
+}
+
+resource "aws_route_table" "Guhatek_route_table" {
+  vpc_id = aws_vpc.Guhatek_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.Guhatek_igw.id
+  }
+
+  tags = {
+    Name = "Guhatek-route-table"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  count          = 2
+  subnet_id      = aws_subnet.Guhatek_subnet[count.index].id
+  route_table_id = aws_route_table.Guhatek_route_table.id
+}
+
+resource "aws_security_group" "Guhatek_cluster_sg" {
+  vpc_id = aws_vpc.Guhatek_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Guhatek-cluster-sg"
+  }
+}
+
+resource "aws_security_group" "Guhatek_node_sg" {
+  vpc_id = aws_vpc.Guhatek_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Guhatek-node-sg"
+  }
+}
+
+resource "aws_eks_cluster" "Guhatek" {
+  name     = "Guhatek-cluster"
+  role_arn = aws_iam_role.Guhatek_cluster_role.arn
+
+  vpc_config {
+    subnet_ids         = aws_subnet.Guhatek_subnet[*].id
+    security_group_ids = [aws_security_group.Guhatek_cluster_sg.id]
+  }
+}
+
+resource "aws_eks_node_group" "Guhatek" {
+  cluster_name    = aws_eks_cluster.Guhatek.name
+  node_group_name = "Guhatek-node-group"
+  node_role_arn   = aws_iam_role.Guhatek_node_group_role.arn
+  subnet_ids      = aws_subnet.Guhatek_subnet[*].id
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
+  }
+
+  instance_types = ["t2.medium"]
+
+  remote_access {
+    ec2_ssh_key               = var.ssh_key_name
+    source_security_group_ids = [aws_security_group.Guhatek_node_sg.id]
+  }
+}
+
+resource "aws_iam_role" "Guhatek_cluster_role" {
+  name = "Guhatek-cluster-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "Guhatek_cluster_role_policy" {
+  role       = aws_iam_role.Guhatek_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role" "Guhatek_node_group_role" {
+  name = "Guhatek-node-group-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "Guhatek_node_group_role_policy" {
+  role       = aws_iam_role.Guhatek_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "Guhatek_node_group_cni_policy" {
+  role       = aws_iam_role.Guhatek_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "Guhatek_node_group_registry_policy" {
+  role       = aws_iam_role.Guhatek_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "null_resource" "configure_kubectl" {
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --region ${var.region} --name ${aws_eks_cluster.Guhatek.name}"
+  }
+
+  depends_on = [aws_eks_cluster.Guhatek]
+}
