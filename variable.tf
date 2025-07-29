93b1d458d29cd2d27b5521db27e791f46077fac2
yathish <yathish1047@gmail.com>
2025-07-29 15:45:44 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,5 +5,5 @@ variable "region" {
 variable "ssh_key_name" {
   description = "The name of the SSH key pair to use for instances"
   type        = string
-  default     = ""
+  default     = "Replace your key-pair"
 }
